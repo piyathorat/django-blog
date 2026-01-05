f5d8a38678ed4c55da083392304fa52a2e635be2
Priya Thorat <160406514+piyathorat@users.noreply.github.com>
2026-01-05 16:42:00 +0530

    blogs app and blog model

---
+++ blogs/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from . models import Category,Blog
+# Register your models here.
+admin.site.register(Category)
+
+
+class BlogAdmin(admin.ModelAdmin):   #its for autognerated slug
+    prepopulated_fields={'slug':('title',)}
+    list_display=('title','category','author','status','is_featured')
+    search_fields=('id','title','category__category_name','status')#its foreignkey field thats why
+    list_editable=('is_featured',)
+
+admin.site.register(Blog,BlogAdmin)
---
+++ blogs/models.py
@@ -0,0 +1,62 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+class Category(models.Model):
+    category_name=models.CharField(max_length=50,unique=True)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+
+    class Meta:  #its for plural categorys to ==
+        verbose_name_plural='categories'
+
+    def __str__(self): #its used for admin panel not to return object1 return name
+        return self.category_name
+    
+STATUS_CHOICES=(
+    ("Draft",'Draft'),
+    ("Published","Published")
+)
+class Blog(models.Model):
+
+    title = models.CharField(max_length=100)
+    # Stores the blog title (e.g., "Introduction to Django")
+
+    slug = models.SlugField(max_length=150, unique=True, blank=True)
+    # URL-friendly version of the title (used in URLs)
+    # Example: "introduction-to-django"
+    # unique=True → no two blogs can have same slug
+    # blank=True → slug can be auto-generated
+
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    # Links blog to a category (one category → many blogs)
+    # CASCADE → delete blogs if category is deleted
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    # Links blog to a user (author)
+    # CASCADE → delete blogs if author is deleted
+
+    featured_img = models.ImageField(upload_to='uploads/%Y/%m/%d')
+    # Stores blog image
+    # Uploaded to: uploads/year/month/day/
+
+    short_description = models.TextField(max_length=500)
+    # Short summary of blog (used in cards & previews)
+
+    blog_body = models.TextField(max_length=2000)
+    # Full blog content/article
+
+    status = models.CharField(max_length=20,choices=STATUS_CHOICES, default="Draft")
+    # Blog status (Draft / Published)
+    # Choices defined in STATUS_CHOICES
+
+    is_featured = models.BooleanField(default=False)
+    # Marks blog as featured (shown on homepage)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    # Stores date & time when blog is created
+
+    updated_at = models.DateTimeField(auto_now=True)
+    # Updates date & time whenever blog is edited
+
+    def __str__(self): #its used for admin panel not to return object1 return name
+        return self.title
